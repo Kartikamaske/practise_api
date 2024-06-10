5cd748da0cb0f93d0716007fdbc8f43e72134658
Kartikamaske <kartikamaske910@gmail.com>
2024-06-10 13:08:17 +0530

    add getcitydata api

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello Pune!')
 })
 
+app.get('/getcitydata', (req, res) => {
+  res.send([{"1":'Kolhapur'},{"2":'Sangli'},{"3":'Satara'},{"4":'Sangli'}])
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
